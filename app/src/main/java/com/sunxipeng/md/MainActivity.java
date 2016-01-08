ac91b1d7f4870a8b9df623fead032db6d2434321
sunxipeng <1141351242@qq.com>
2016-01-08 14:08:55 +0800

    Initial commit

---
+++ app/src/main/java/com/sunxipeng/md/MainActivity.java
@@ -0,0 +1,121 @@
+package com.sunxipeng.md;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    DrawerLayout mDrawerLayout;
+
+    ActionBarDrawerToggle mDrawerToggle;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+
+        listView = (ListView) findViewById(R.id.list_view);
+
+        listView.setAdapter(new ContentAdapter());
+
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        // getActionBar().setTitle("开头");  这个不能用，getActionBar返回空。
+        getSupportActionBar().setTitle("开头");
+    }
+
+
+    /*private class MyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+
+            return getLayoutInflater().inflate(R.layout.test,null);
+        }
+    }*/
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        mDrawerToggle.syncState();// 这个必须要，没有的话进去的默认是个箭头。。正常应该是三横杠的
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return mDrawerToggle.onOptionsItemSelected(item)
+                || super.onOptionsItemSelected(item);
+    }
+
+
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private class ContentAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return 9;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            convertView = getLayoutInflater().inflate(R.layout.content,null);
+
+
+            return convertView;
+        }
+    }
+}
